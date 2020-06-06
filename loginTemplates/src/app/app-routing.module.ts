7136e827b058c90dc2eff092d187d338ca41200c
Alexander <alextamayop@gmail.com>
2020-06-05 22:33:09 -0500

    Create basic structure for basic login with tree menus to call events

--- loginTemplates/src/app/app-routing.module.ts
+++ loginTemplates/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BasicLoginComponent } from './login/basic-login/basic-login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login/basic', component: BasicLoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- loginTemplates/src/app/app.module.ts
+++ loginTemplates/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasicLoginComponent } from './login/basic-login/basic-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicLoginComponent
   ],
   imports: [
     BrowserModule,
